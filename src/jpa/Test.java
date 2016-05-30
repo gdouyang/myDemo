81e234697d293a0a77b528dfbbcb75c26613cd3c
Snake <Snake@gdouyang-mbp.local>
2016-05-30 22:38:00 +0800

    去掉重复jar包

---
+++ src/jpa/Test.java
@@ -0,0 +1,14 @@
+package jpa;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("/application.xml");
+//		context.
+		CustomerRepository repository = context.getBean(CustomerRepository.class);
+	}
+}
