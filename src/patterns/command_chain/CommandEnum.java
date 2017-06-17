dfc5691d907935933f2247dd8e01c5653954c4d8
Administrator <Administrator@PC-201612091935>
2017-06-17 19:45:20 +0800

    修改编译错误

--- src/patterns/command_chain/CommandEnum.java
+++ src/patterns/command_chain/CommandEnum.java
@@ -3,30 +3,25 @@ package patterns.command_chain;
 import java.util.ArrayList;
 import java.util.List;
 
-public enum CommandEnum
-{
-	ls("com.cbf4life.common.command.LSCommand");
-	
+public enum CommandEnum {
+	ls("patterns.command_chain.LSCommand");
+
 	private String value = "";
-	
+
 	// 定义构造函数，目的是Data(value)类型的相匹配
-	private CommandEnum(String value)
-	{
+	private CommandEnum(String value) {
 		this.value = value;
 	}
-	
-	public String getValue()
-	{
+
+	public String getValue() {
 		return this.value;
 	}
-	
+
 	// 返回所有的enum对象
-	public static List<String> getNames()
-	{
+	public static List<String> getNames() {
 		CommandEnum[] commandEnum = CommandEnum.values();
 		List<String> names = new ArrayList<String>();
-		for (CommandEnum c : commandEnum)
-		{
+		for (CommandEnum c : commandEnum) {
 			names.add(c.name());
 		}
 		return names;
